949edeaeb30b958b57322c586fe35bf136200086
Jose Vicente <al2212002118@azc.uam.mx>
2024-09-22 12:25:28 -0600

    Asiento
    Modifique para que coincida con la BD

--- src/main/java/uam/azc/adsi/cinetubi/model/Pelicula.java
+++ src/main/java/uam/azc/adsi/cinetubi/model/Pelicula.java
@@ -7,33 +7,29 @@ package uam.azc.adsi.cinetubi.model;
 /**
  *
  * @author Jony
+ * @author José Vicente López López 2212002118
  */
 class Pelicula {
     private int idPelicula;
     private String titulo;
-    private String descripcion;
-    private String genero;
     private int duracion;
-    private String idioma;
+    private String distribuidor;
     private String clasificacion;
+    private String director;
+    private String pais;
+    private String descripcion;
 
-    public Pelicula() {
-    }
-
-    public Pelicula(int idPelicula, String titulo, String descripcion, String genero, int duracion, String idioma, String clasificacion) {
+    public Pelicula(int idPelicula, String titulo, int duracion, String distribuidor, String clasificacion, String director, String pais, String descripcion) {
         this.idPelicula = idPelicula;
         this.titulo = titulo;
-        this.descripcion = descripcion;
-        this.genero = genero;
         this.duracion = duracion;
-        this.idioma = idioma;
+        this.distribuidor = distribuidor;
         this.clasificacion = clasificacion;
+        this.director = director;
+        this.pais = pais;
+        this.descripcion = descripcion;
     }
 
-    
-    
-    
-    
     public int getIdPelicula() {
         return idPelicula;
     }
@@ -50,22 +46,6 @@ class Pelicula {
         this.titulo = titulo;
     }
 
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
     public int getDuracion() {
         return duracion;
     }
@@ -74,12 +54,12 @@ class Pelicula {
         this.duracion = duracion;
     }
 
-    public String getIdioma() {
-        return idioma;
+    public String getDistribuidor() {
+        return distribuidor;
     }
 
-    public void setIdioma(String idioma) {
-        this.idioma = idioma;
+    public void setDistribuidor(String distribuidor) {
+        this.distribuidor = distribuidor;
     }
 
     public String getClasificacion() {
@@ -89,7 +69,34 @@ class Pelicula {
     public void setClasificacion(String clasificacion) {
         this.clasificacion = clasificacion;
     }
-    
-    
-    
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" + "idPelicula=" + idPelicula + ", titulo=" + titulo + ", duracion=" + duracion + ", distribuidor=" + distribuidor + ", clasificacion=" + clasificacion + ", director=" + director + ", pais=" + pais + ", descripcion=" + descripcion + '}';
+    }
+
 }
