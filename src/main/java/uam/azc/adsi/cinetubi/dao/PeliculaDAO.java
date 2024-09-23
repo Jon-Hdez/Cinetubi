09b7ccb1aeb042c357ff6627f63de7f356dc1698
Jon-Hdez <al2182004371@azc.uam.mx>
2024-09-23 00:01:43 -0600

    Add files via upload

--- src/main/java/uam/azc/adsi/cinetubi/dao/PeliculaDAO.java
+++ src/main/java/uam/azc/adsi/cinetubi/dao/PeliculaDAO.java
@@ -4,61 +4,83 @@
  */
 package uam.azc.adsi.cinetubi.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import uam.azc.adsi.cinetubi.model.*;
-import uam.azc.adsi.cinetubi.util.DatabaseConnection;
+import uam.azc.adsi.cinetubi.model.Pelicula;
 
-/**
- *
- * @author vicen
- */
+
 public class PeliculaDAO {
     private Connection connection;
 
-    private static final String URL = "jdbc:mysql://localhost:3306/cinetubi";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    public PeliculaDAO(Connection connection) {
+        this.connection = connection;
+    }
 
-    public PeliculaDAO() {
-        try {
-            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException ex) {
-            System.err.println("Error tipo: " +ex);
+    // Crear película
+    public boolean agregarPelicula(Pelicula pelicula) throws SQLException {
+        String query = "INSERT INTO pelicula (titulo, duracion, distribuidor, clasificacion, director, pais, descripcion) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setString(1, pelicula.getTitulo());
+            stmt.setInt(2, pelicula.getDuracion());
+            stmt.setString(3, pelicula.getDistribuidor());
+            stmt.setString(4, pelicula.getClasificacion());
+            stmt.setString(5, pelicula.getDirector());
+            stmt.setString(6, pelicula.getPais());
+            stmt.setString(7, pelicula.getDescripcion());
+            return stmt.executeUpdate() > 0;
         }
-      
     }
-    
-    public Pelicula searchMovie(int idP){
-        try {
-            String sql = "SELECT * FROM pelicula WHERE id="+idP +";";
-            PreparedStatement ps;
-            ps = connection.prepareStatement(sql);
-            
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                int id =rs.getInt(1);
-                String titulo = rs.getString(2);
-                int duracion=rs.getInt(3);
-                String distribuidor=rs.getString(4);
-                String clasifiacion=rs.getString(5);
-                String director=rs.getString(6);
-                String pais=rs.getString(7);
-                String descripcion=rs.getString(8);
-                Pelicula pelicula = new Pelicula(id,titulo, duracion, distribuidor, clasifiacion, director, pais, descripcion);
-                return pelicula;
+
+    // Leer todas las películas
+    public List<Pelicula> obtenerPeliculas() throws SQLException {
+        List<Pelicula> peliculas = new ArrayList<>();
+        String query = "SELECT * FROM pelicula";
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
+                Pelicula pelicula = new Pelicula();
+                pelicula.setId(rs.getInt("id"));
+                pelicula.setTitulo(rs.getString("titulo"));
+                pelicula.setDuracion(rs.getInt("duracion"));
+                pelicula.setDistribuidor(rs.getString("distribuidor"));
+                pelicula.setClasificacion(rs.getString("clasificacion"));
+                pelicula.setDirector(rs.getString("director"));
+                pelicula.setPais(rs.getString("pais"));
+                pelicula.setDescripcion(rs.getString("descripcion"));
+                peliculas.add(pelicula);
             }
-        } catch (SQLException ex) {
-            System.err.println("Error tipo: " +ex);
         }
-        return null;
+        return peliculas;
     }
-    
+
+    // Actualizar película
+    public void actualizarPelicula(Pelicula pelicula) throws SQLException {
+        String query = "UPDATE pelicula SET titulo = ?, duracion = ?, distribuidor = ?, clasificacion = ?, director = ?, pais = ?, descripcion = ? WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setString(1, pelicula.getTitulo());
+            stmt.setInt(2, pelicula.getDuracion());
+            stmt.setString(3, pelicula.getDistribuidor());
+            stmt.setString(4, pelicula.getClasificacion());
+            stmt.setString(5, pelicula.getDirector());
+            stmt.setString(6, pelicula.getPais());
+            stmt.setString(7, pelicula.getDescripcion());
+            stmt.setInt(8, pelicula.getId());
+            
+            int filasActualizadas = stmt.executeUpdate();
+
+            if (filasActualizadas == 0) {
+                throw new SQLException("No se pudo actualizar la película, no se encontró el ID.");
+            }
+        }
+    }
+
+    // Eliminar película
+    public boolean eliminarPelicula(int id) throws SQLException {
+        String query = "DELETE FROM pelicula WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            return stmt.executeUpdate() > 0;
+        }
+    }
 }
